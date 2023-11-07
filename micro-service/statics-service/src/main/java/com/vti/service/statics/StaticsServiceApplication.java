00b87d06882f3114f5bf7f34f31ee9131d3bbcda
chienVti <chien.tranvan@vti.com.vn>
2023-11-07 19:12:01 +0900

    initial project

---
+++ micro-service/statics-service/src/main/java/com/vti/service/statics/StaticsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vti.service.statics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticsServiceApplication.class, args);
+	}
+
+}
---
+++ micro-service/statics-service/src/test/java/com/vti/service/statics/StaticsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vti.service.statics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
